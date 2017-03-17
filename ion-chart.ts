18fd53b690b4516797924c9ae62717cba36e5173
Marcos Paiva <marckpaulos@hotmail.com>
2017-03-17 00:28:47 -0300

    Char component adjustments

---
+++ ion-chart.ts
@@ -0,0 +1,140 @@
+import { Component, Input, Output, ViewChild  } from '@angular/core';
+import { Chart } from 'chart.js';
+
+/*
+  Generated class for the IonChart component.
+
+  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+  for more info on Angular 2 Components.
+*/
+@Component({
+  selector: 'ion-chart',
+  template: `
+  <ion-card>
+       <ion-card-header>
+        {{headerTitle}}
+       </ion-card-header>
+       <ion-card-content>
+         <canvas #baseCanvas></canvas>
+       </ion-card-content>
+     </ion-card>
+ `,
+})
+export class IonChartComponent {
+debugger
+  @Input('headerTitle') textHeader;
+  @Input('type') chartType;
+  @ViewChild('baseCanvas') baseCanvas;
+
+  baseChart: any;
+  linechart: any;
+  headerTitle :string;
+
+  constructor() {
+
+  }
+
+  ngOnInit () {
+    var self = this;
+if(self.chartType == 'bar'){
+    self.baseChart = new Chart(this.baseCanvas.nativeElement, {
+
+              type: self.chartType,
+              data: {
+                  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                  datasets: [{
+                      label: 'Accesses',
+                      data: [20, 19, 3, 5, 2, 3, 8, 10, 6, 15, 16, 18],
+                      backgroundColor: [
+                          'rgba(255, 99, 50, 1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)',
+                          'rgba(236, 85, 90, 1)',
+                          'rgba(90, 37, 64, 1)',
+                          'rgba(85, 159, 15, 1)',
+                          'rgba(77, 156, 62, 1)',
+                          'rgba(145, 159, 85, 1)',
+                          'rgba(176, 110, 68, 1)'
+                      ],
+                      borderColor: [
+                          'rgba(255,99,50,1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)',
+                          'rgba(236, 85, 90, 1)',
+                          'rgba(90, 37, 64, 1)',
+                          'rgba(85, 159, 15, 1)',
+                          'rgba(77, 156, 62, 1)',
+                          'rgba(145, 159, 85, 1)',
+                          'rgba(176, 110, 68, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  scales: {
+                    xAxes: [{
+                      gridLines: {
+                          display:false
+                      }
+                     }],
+                    yAxes: [{
+                        gridLines: {
+                             display:false
+                          },
+                          ticks: {
+                              beginAtZero:true
+                          }
+                    }]
+                  }
+              }
+
+          });
+        }
+        else if(this.chartType == 'line'){
+
+        this.linechart = new Chart(this.baseCanvas.nativeElement, {
+
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "Accesses",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+
+        });
+        }
+
+  }
+  ngAfterViewInit(){
+      this.headerTitle = this.textHeader;
+  }
+
+}
