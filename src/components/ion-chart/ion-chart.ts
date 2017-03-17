971f415ff4e0c662e887ae9a71655b65e57492f5
Marcos Paiva <marckpaulos@hotmail.com>
2017-03-17 00:31:24 -0300

    Update ion-chart

--- src/components/ion-chart/ion-chart.ts
+++ src/components/ion-chart/ion-chart.ts
@@ -27,8 +27,8 @@ debugger
   @ViewChild('baseCanvas') baseCanvas;
 
   baseChart: any;
+  linechart: any;
   headerTitle :string;
-
 
   constructor() {
 
@@ -36,6 +36,7 @@ debugger
 
   ngOnInit () {
     var self = this;
+if(self.chartType == 'bar'){
     self.baseChart = new Chart(this.baseCanvas.nativeElement, {
 
               type: self.chartType,
@@ -94,6 +95,42 @@ debugger
               }
 
           });
+        }
+        else if(this.chartType == 'line'){
+
+        this.linechart = new Chart(this.baseCanvas.nativeElement, {
+
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "Accesses",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+
+        });
+        }
 
   }
   ngAfterViewInit(){
