a178f26c663850c512caf755966ccd7db514995d
Marcos Paiva <marckpaulos@hotmail.com>
2017-03-18 00:46:08 -0300

    Add pie chart in component

--- src/components/ion-chart/ion-chart.ts
+++ src/components/ion-chart/ion-chart.ts
@@ -27,7 +27,6 @@ debugger
   @ViewChild('baseCanvas') baseCanvas;
 
   baseChart: any;
-  linechart: any;
   headerTitle :string;
 
   constructor() {
@@ -98,7 +97,7 @@ if(self.chartType == 'bar'){
         }
         else if(this.chartType == 'line'){
 
-        this.linechart = new Chart(this.baseCanvas.nativeElement, {
+        this.baseChart = new Chart(this.baseCanvas.nativeElement, {
 
             type: 'line',
             data: {
@@ -130,6 +129,36 @@ if(self.chartType == 'bar'){
             }
 
         });
+        }
+        else if(this.chartType == 'pie'){
+          this.baseChart = new Chart(this.baseCanvas.nativeElement, {
+
+           type: 'pie',
+           data: {
+               labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+               datasets: [{
+                   label: '# of Votes',
+                   data: [12, 19, 3, 5, 2, 3],
+                   backgroundColor: [
+                       'rgba(255, 99, 132, 0.2)',
+                       'rgba(54, 162, 235, 0.2)',
+                       'rgba(255, 206, 86, 0.2)',
+                       'rgba(75, 192, 192, 0.2)',
+                       'rgba(153, 102, 255, 0.2)',
+                       'rgba(255, 159, 64, 0.2)'
+                   ],
+                   hoverBackgroundColor: [
+                       "#FF6384",
+                       "#36A2EB",
+                       "#FFCE56",
+                       "#FF6384",
+                       "#36A2EB",
+                       "#FFCE56"
+                   ]
+               }]
+           }
+
+       });
         }
 
   }
